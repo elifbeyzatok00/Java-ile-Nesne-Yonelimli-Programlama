27bae2a0c1cdf8770ef06ed9a6b76cfd4d60e5fc
Elif Beyza Tok <102792446+elifbeyzatok00@users.noreply.github.com>
2023-11-24 14:39:45 +0300

    Update Main.java

--- 01-Object-Oriented-Programming-Philosophy/Senaryo1/Main.java/src/Main.java
+++ 01-Object-Oriented-Programming-Philosophy/Senaryo1/Main.java/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Car car = new Car();
         // ='in sağında new Car() ile RAM'de bir yer ayırdık, yeni nesne oluşturduk, memory allocate edildi yani bellek tahsisi yapıldı.
         // ='in solunda ise car değişkeni ile bu nesneye referans verdik.
-        // car yerine başka isim de verebilirdik. Ama Car caryazdık çünkü bu best practise'dir. Yani birden fazla alternatifi olsa da daha çok bu kullanılır. 
+        // car yerine başka isim de verebilirdik. Ama Car car yazdık çünkü bu best practise'dir. Yani birden fazla alternatifi olsa da daha çok bu kullanılır. 
         // burada önemli olan en baştaki Car yazısı. Bu nesnenin Car sınıfından oluşturulduğunu gösterir.
 
         Car car2 = new Car();
